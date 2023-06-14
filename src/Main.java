470b461b6e29498b3653249feb9f1d85aeb99198
Arifozden <111346692+Arifozden@users.noreply.github.com>
2023-06-14 14:07:20 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        double c;
+
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("1.kenari giriniz");
+        a=girdi.nextInt();
+        System.out.print("2.kenari giriniz");
+        b=girdi.nextInt();
+
+        c=Math.sqrt((a*a)+(b*b));
+        System.out.println("Hipotenus : "+c);
+    }
+}
